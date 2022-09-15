e19ac2334a5101d814c15052d77502c48646e360
psegarra <pausegarra.b@gmail.com>
2022-09-15 20:07:42 +0200

    🎨: searchbar styles

--- src/components/SearchBar.svelte
+++ src/components/SearchBar.svelte
@@ -2,4 +2,6 @@
   export let setSearch
 </script>
 
-<input type="text" on:input={setSearch} />
+<div class="py-5">
+  <input type="text" on:input={setSearch} class="w-full py-1 px-2" placeholder="Start typing to search!" />
+</div>
