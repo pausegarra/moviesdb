9f94a1f20b080b3bc55406e969b22b3fa23f9ea2
psegarra <pausegarra.b@gmail.com>
2022-09-15 21:03:11 +0200

    🎨: styling the single page of the movie

--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -3,4 +3,14 @@
   export let data;
 </script>
 
-{data.Title}
+<div class="container mx-auto py-9 px-4 md:px-0">
+  <h1 class="text-5xl mb-9 font-bold text-center">{data.Title}</h1>
+
+  <div class="columns-2 p-8 bg-white rounded">
+    <div class="flex justify-center items-center">
+      <img src={data.Poster} alt="">
+    </div>
+    <div class="flex justify-center">
+    </div>
+  </div>
+</div>
