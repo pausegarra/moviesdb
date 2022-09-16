2b30959c0b559f2b1b086eddc3de25577456dc07
psegarra <pausegarra.b@gmail.com>
2022-09-16 21:26:31 +0200

    🎨: added footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,3 +5,10 @@
 <div>
   <slot />
 </div>
+
+<footer class="flex items-center flex-col">
+  <div>developed by pau<strong>segarra</strong></div>
+  <div class="flex gap-5 py-5">
+    <a href="https://github.com/pausegarra/moviesdb.git" target="_blank"><img src="/github.svg" class="opacity-50" alt=""></a>
+  </div>
+</footer>
