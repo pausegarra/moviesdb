8fa4654e0bcd7626235de78bf1c147a9d2d429e9
psegarra <pausegarra.b@gmail.com>
2022-09-16 20:28:47 +0200

    🎨: changed card hover and title size

--- src/components/Movie.svelte
+++ src/components/Movie.svelte
@@ -11,12 +11,12 @@
 
 <a
 	href={`${movie.imdbID}`}
-	class="block bg-[#FFFFFF] hover:bg-[#F7CE68] ease-in-out duration-200 rounded-lg shadow-md mb-4 overflow-hidden"
+	class="block bg-[#FFFFFF] hover:bg-[#000000]/25 ease-in-out duration-200 rounded-lg shadow-md mb-4 overflow-hidden"
 >
 	<img src={movie.Poster} alt="" class="w-full" />
 	<div class="p-6">
     <span class="bg-red-200 text-red-800 px-2.5 rounded py-1 text-xs">{movie.Year}</span>
 		<Badge movieType={movie.Type} />
-		<h4 class="m-0 font-bold text-2xl">{movie.Title}</h4>
+		<h4 class="m-0 font-bold text-xl">{movie.Title}</h4>
 	</div>
 </a>
