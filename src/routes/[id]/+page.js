ad995917409d5b1819f860a59830eb3227a314f1
psegarra <pausegarra.b@gmail.com>
2022-09-17 09:57:22 +0200

    ♻️: changed window.fetch for the one recieved in load function

--- src/routes/[id]/+page.js
+++ src/routes/[id]/+page.js
@@ -1,7 +1,7 @@
 import { error } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageLoad} */
-export async function load({ params }) {
+export async function load({ params, fetch }) {
 	const res = await fetch(`https://www.omdbapi.com/?apikey=11aa9d2f&i=${params.id}`);
 	const data = await res.json();
 	return data;
