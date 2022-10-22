5dbe31505bfc8e9d4f4cc63d3727633b8da82380
psegarra <pausegarra.b@gmail.com>
2022-10-22 13:59:58 +0200

    🎨 removed opacity of svg

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,6 +9,6 @@
 <footer class="flex items-center flex-col">
   <div class="text-[#00004c]">developed by pau<strong>segarra</strong></div>
   <div class="flex gap-5 py-5">
-    <a href="https://github.com/pausegarra/moviesdb.git" target="_blank"><img src="/github.svg" class="opacity-50" alt=""></a>
+    <a href="https://github.com/pausegarra/moviesdb.git" target="_blank"><img src="/github.svg" alt=""></a>
   </div>
 </footer>
